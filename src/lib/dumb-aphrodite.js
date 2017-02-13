c4c5a967dea44720f7be1c928eaa887181b68890
Sascha Gehlich <sascha@gehlich.us>
2017-02-13 16:14:38 +0100

    :bug: dumb-aphrodite is now a little smarter

--- src/lib/dumb-aphrodite.js
+++ src/lib/dumb-aphrodite.js
@@ -1,12 +1,16 @@
 export function css (...styles) {
-  return styles.join('-o_O-')
+  return styles
+    .map(s => s._name)
+    .join('-o_O-')
 }
 
 export class StyleSheet {
+  // Input: { button: 'abcdef', primary: 'abcdef' }
+  // Output: { button: { _name: 'abcdef' }, primary: { _name: 'abcdef' }}
   static create (styles) {
     const stylesObject = {}
     for (let key in styles) {
-      stylesObject[key] = `${key}_${styles[key]}`
+      stylesObject[key] = { _name: `${key}_${styles[key]}` }
     }
     return stylesObject
   }
