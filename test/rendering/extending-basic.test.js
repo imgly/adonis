8a72ae0c6e934c3652d342eda876e427463a5bcc
Sascha Gehlich <sascha@gehlich.us>
2017-03-20 15:26:55 +0100

    :racehorse: Reduced amount of generated CSS

--- test/rendering/extending-basic.test.js
+++ test/rendering/extending-basic.test.js
@@ -17,7 +17,7 @@ describe('extending an adonis component', () => {
     const { html, css } = render(adonis, <OverriddenWrapper />)
 
     html.should.equal('<div class="div~tcorpq__AdonisComponent~1nxhvta"></div>')
-    css.content.should.equal(`.AdonisComponent~1nxhvta {\n  background: blue;\n}\n\n.div~tcorpq__AdonisComponent~1nxhvta {\n  background: blue;\n  padding: 5px;\n}`)
+    css.content.should.equal(`.div~tcorpq__AdonisComponent~1nxhvta {\n  background: blue;\n  padding: 5px;\n}`)
   })
 
   describe('when passing a name', () => {
@@ -32,7 +32,7 @@ describe('extending an adonis component', () => {
       const { html, css } = render(adonis, <OverriddenWrapper />)
 
       html.should.equal('<div class="Wrapper~tcorpq__OverriddenWrapper~1nxhvta"></div>')
-      css.content.should.equal(`.OverriddenWrapper~1nxhvta {\n  background: blue;\n}\n\n.Wrapper~tcorpq__OverriddenWrapper~1nxhvta {\n  background: blue;\n  padding: 5px;\n}`)
+      css.content.should.equal(`.Wrapper~tcorpq__OverriddenWrapper~1nxhvta {\n  background: blue;\n  padding: 5px;\n}`)
     })
   })
 })
