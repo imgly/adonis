6ff7d47e7317198d941f7c038f86f0d3dea336e7
Sascha Gehlich <sascha@gehlich.us>
2017-03-21 09:13:19 +0100

    :sparkles: Implement CSS vendor prefixes

--- src/lib/styles/rule.js
+++ src/lib/styles/rule.js
@@ -1,15 +1,34 @@
+import CSSPrefixes from '../css-prefixes'
+
 export default class Rule {
-  constructor (key, value) {
+  constructor (adonis, key, value) {
+    this._adonis = adonis
     this.key = key
     this.value = value
+    this.cssKey = this.key.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`)
   }
 
-  getCSSKey () {
-    return this.key.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`)
+  _getAutoPrefixedKeyPairs () {
+    const prefixes = CSSPrefixes[this.key]
+    const keyPairs = []
+    if (prefixes) {
+      prefixes.forEach((prefix) => {
+        keyPairs.push([`-${prefix}-${this.cssKey}`, this.value])
+      })
+    }
+    return keyPairs
   }
 
-  toCSS (minified = false) {
-    const key = this.getCSSKey()
-    return `${key}:${minified ? '' : ' '}${this.value};`
+  toCSS () {
+    const { minified, autoPrefix } = this._adonis.getOptions()
+
+    let keyPairs = [[this.cssKey, this.value]]
+    if (autoPrefix) {
+      keyPairs = keyPairs.concat(this._getAutoPrefixedKeyPairs())
+    }
+
+    return keyPairs.map(([key, value]) => {
+      return `${minified ? '' : '  '}${key}:${minified ? '' : ' '}${value};`
+    }).join(minified ? '' : '\n')
   }
 }
