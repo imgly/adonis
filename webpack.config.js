e857c8cabd830ba421252fbac48556d6f5aac0b2
Sascha Gehlich <sascha@gehlich.us>
2017-04-26 10:47:08 +0200

    :bug: Fix adonis usage in browser

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,23 @@ module.exports = {
   output: {
     path: './build',
     filename: 'adonis' + (MINIFY ? '.min.js' : '.js'),
-    library: 'adonis',
+    library: 'Adonis',
     libraryTarget: 'umd'
   },
 
   externals: {
-    react: 'react'
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
   },
 
   resolve: {
